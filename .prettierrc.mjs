63502226a9dabd77b501e038162cbcf8e4bcdce3
arek-3share <arkadiusz.wojda@3sharecorp.com>
2025-11-14 18:07:12 +0100

    prettier updates to the initial structure

---
+++ .prettierrc.mjs
@@ -0,0 +1,6 @@
+/** @type {import("prettier").Config} */
+const config = {
+  trailingComma: "none"
+};
+
+export default config;
--- .prettierrc.ts
+++
@@ -1,7 +0,0 @@
-import { type Config } from "prettier";
-
-const config: Config = {
-  trailingComma: "none",
-};
-
-module.exports = config;
